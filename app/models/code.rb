f4e0f8a9d6ca28de6d0fbb07580675255b0467b6
Jacky Li <veryfaint@gmail.com>
2011-03-30 23:20:24 +0800

    remove useless methods

--- app/models/code.rb
+++ app/models/code.rb
@@ -8,14 +8,6 @@ class Code
     @coverage = coverage
   end
   
-  def complexity_tip
-    name + ": commit=> " + (commit.to_s) + " complexity=> " + (complexity.to_s)
-  end
-  
-  def coverage_tip
-    name + ": commit=> " + (commit.to_s) + " complexity=> " + (coverage.to_s)
-  end
-  
   def tip
     name + ": commit=> " + (commit.to_s) + " complexity=> " + (complexity.to_s) + " coverage=> " + (coverage.to_s)
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -12,28 +12,4 @@ class Project
   def read measurement_parser
     measurement_parser.parse(@name)
   end
-  
-  def code_complexity
-    codes = {}
-    complexity_file = File.join(DDR_ENV[:project_root], @name, "complexity.txt")
-    File.open(complexity_file) { |f|
-      while line = f.gets
-        tmp = line.split(":")
-        codes[tmp[0]] = Code.new(tmp[0], 0, tmp[1].to_i, 0)
-      end
-    } 
-    codes
-  end
-  
-  def code_coverage
-    codes = {}
-    coverage_file = File.join(DDR_ENV[:project_root], @name, "coverage.txt")
-    File.open(coverage_file) { |f|
-      while line = f.gets
-        tmp = line.split(":")
-        codes[tmp[0]] = Code.new(tmp[0], 0, 0, tmp[1].to_f)
-      end
-    } 
-    codes
-  end
 end
